e8105d879666ce53c501a7c3afebd5b8e4045f83
Zzzackk <isaacvono@outlook.com>
2026-03-12 23:17:10 -0300

    Override toString method and modify its return

--- the-hungry-developer/app/src/main/java/com/isaacvono/thehungrydeveloper/Dish.java
+++ the-hungry-developer/app/src/main/java/com/isaacvono/thehungrydeveloper/Dish.java
@@ -1,5 +1,7 @@
 package com.isaacvono.thehungrydeveloper;
 
+import androidx.annotation.NonNull;
+
 public class Dish {
 
     //Fields
@@ -7,11 +9,16 @@ public class Dish {
     String description;
     int price;
 
+    // Constructor
     Dish(String title, String description, int price) {
         this.title = title;
         this.description = description;
         this.price = price;
     }
 
-
+    @NonNull
+    @Override
+    public String toString() {
+        return title;
+    }
 }
