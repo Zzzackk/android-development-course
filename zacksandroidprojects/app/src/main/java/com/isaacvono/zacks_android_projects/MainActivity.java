dc9401347eee574e99f6272d77d4d6251fde1943
Zzzackk <isaacvono@outlook.com>
2026-03-18 19:45:32 -0300

    Create project adapter and integrate it in main activity

--- zacksandroidprojects/app/src/main/java/com/isaacvono/zacks_android_projects/MainActivity.java
+++ zacksandroidprojects/app/src/main/java/com/isaacvono/zacks_android_projects/MainActivity.java
@@ -9,21 +9,23 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
         Project[] projects = {
                 new Project("Getting Started App", "My very first 'Hello World' app. (The default Android Studio template project", R.drawable.getting_started),
-                new Project("Zacks Quote", "making a simple change to the layout with my own personal motivational quote for software developers", R.drawable.quote),
+                new Project("Zacks Quote", "Making a simple change to the layout with my own personal motivational quote for software developers", R.drawable.quote),
                 new Project("BMI Calculator", "A real life working BMI calculator teaching me Variables, Methods and Conditional logic", R.drawable.calculator),
                 new Project("Inches Converter", "A basic converter to convert inches to meters(my own personal calculator app made on my own)", R.drawable.tape),
                 new Project("The Hungry Developer", "A menu app for a fictional restaurant, learning about Activities, Classes and Objects, as well as Array Adapters and much more", R.drawable.hungry_developer)
         };
 
-        ArrayAdapter<Project> projectsAdapter = new ArrayAdapter<>(this, R.layout.item_project, projects);
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+
+        list.setAdapter(adapter);
     }
 }
--- zacksandroidprojects/app/src/main/java/com/isaacvono/zacks_android_projects/Project.java
+++ zacksandroidprojects/app/src/main/java/com/isaacvono/zacks_android_projects/Project.java
@@ -14,7 +14,4 @@ public class Project {
         this.image = image;
     }
 
-    // Methods
-
-
 }
