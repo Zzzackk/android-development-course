1ac56d403c27b928755aaed0ecb1f851d167b20c
Isaac Vono <isaacvono@outlook.com>
2026-03-19 23:56:37 -0300

    Begin logic integration and continue developing layouts

--- zacks-bucket-list/app/src/main/java/com/isaacvono/zacks_bucket_list/MainActivity.java
+++ zacks-bucket-list/app/src/main/java/com/isaacvono/zacks_bucket_list/MainActivity.java
@@ -1,13 +1,44 @@
 package com.isaacvono.zacks_bucket_list;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class MainActivity extends AppCompatActivity {
+
+    MaterialCardView thingsToDoCardView;
+    MaterialCardView placesToGoCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        thingsToDoCardView = findViewById(R.id.card_view_things);
+        placesToGoCardView = findViewById(R.id.card_view_places);
+
+        thingsToDoCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent thingsToDoActivityIntent = new Intent(MainActivity.this, ThingsToDoActivity.class);
+
+                setIntent(thingsToDoActivityIntent);
+
+            }
+        });
+
+        placesToGoCardView.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent placesToGoActivityIntent = new Intent(MainActivity.this, PlacesToGoActivity.class);
+
+                setIntent(placesToGoActivityIntent);
+            }
+        }));
+
     }
 }
---
+++ zacks-bucket-list/app/src/main/java/com/isaacvono/zacks_bucket_list/PlacesToGoActivity.java
@@ -0,0 +1,14 @@
+package com.isaacvono.zacks_bucket_list;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlacesToGoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_to_go);
+    }
+}
