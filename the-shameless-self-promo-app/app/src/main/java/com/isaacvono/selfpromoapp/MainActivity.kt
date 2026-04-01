111e25c3f11da5d13ac6bd3e8c1f600e1f2b54a1
Zzzackk <isaacvono@outlook.com>
2026-04-01 18:01:57 -0300

    Create simple button logic

--- the-shameless-self-promo-app/app/src/main/java/com/isaacvono/selfpromoapp/MainActivity.kt
+++ the-shameless-self-promo-app/app/src/main/java/com/isaacvono/selfpromoapp/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.isaacvono.selfpromoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val previewButton: Button = findViewById(R.id.button_preview)
+        previewButton.setOnClickListener {
+            onPreviewClicked()
+        }
+    }
+
+    fun onPreviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name)
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number)
+        val testString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString()
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
     }
 }
